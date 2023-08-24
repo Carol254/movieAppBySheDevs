75a205c78a77d7d642f29b028f369108ec75f8fb
Caroline <wanguic49@gmail.com>
2023-08-24 15:14:41 +0300

    few changes made

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
+const container = document.querySelector('.parent');
 const seats = document.querySelectorAll(".child:not(.occupied)");
 const numberOfSeats = document.getElementById("numberofSeats");
+const amount = document.getElementById("amount");
+const movie = document.getElementById("movie");
 
-seats.forEach(seat =>{
-    seat.addEventListener("click",selectSeat);
+let ticketTotalPrice = +movie.value;
+
+// seats.forEach(seat =>{
+//     seat.addEventListener("click",selectSeat);
+// })
+
+// function selectSeat(){
+//     alert('you have selected a seat');
+//     console.log('seat selected');
+// }
+
+container.addEventListener('click', e =>{
+   if(e.target.classList.contains('child') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+   }
 })
-
-function selectSeat(){
-    alert('you have selected a seat');
-    console.log('seat selected');
-}
